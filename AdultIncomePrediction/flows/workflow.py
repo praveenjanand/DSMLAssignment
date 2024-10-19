fd7d12883832f61c81bc1d935be48ffbb71e7343
Praveen Anand <jpraveenanand87@gmail.com>
2024-10-19 11:34:13 +0530

    Added Random forest and logistic regression

--- AdultIncomePrediction/flows/workflow.py
+++ AdultIncomePrediction/flows/workflow.py
@@ -4,8 +4,12 @@ from prefect import flow, task
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,precision_score, recall_score,f1_score
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
 
 
 # Step 2: Load the Dataset
@@ -92,13 +96,53 @@ def train_model(df):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = LogisticRegression(max_iter=200)  # Increase max_iter if needed
+    model = LogisticRegression(max_iter=200)  
     model.fit(X_train, y_train)
 
-    # Evaluate the model
     y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
 
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
+    print('Metrics for Logistic Regression')
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+    print(f'Precision: {precision * 100:.2f}%')
+    print(f'Recall: {recall * 100:.2f}%')
+    print(f'F1 Score: {f1 * 100:.2f}%')
+
+    clf = DecisionTreeClassifier()
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    
+    print('Metrics for Decision Tree')
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+    print(f'Precision: {precision * 100:.2f}%')
+    print(f'Recall: {recall * 100:.2f}%')
+    print(f'F1 Score: {f1 * 100:.2f}%')
+
+    clf = RandomForestClassifier()
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    
+    print('Metrics for Random Forest')
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+    print(f'Precision: {precision * 100:.2f}%')
+    print(f'Recall: {recall * 100:.2f}%')
+    print(f'F1 Score: {f1 * 100:.2f}%')
     return accuracy
 
 # Step 5: Define Prefect Flow
